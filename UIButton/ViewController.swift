ad2a4b167ccb2a051a093cd64455fb1c7ddc1eda
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-12 13:13:54 +0300

    init

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .orange
+        button.layer.cornerRadius = 25
+        button.setTitle("Change", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(changeLabel), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
+    }
+    
+    func setUI() {
+        view.addSubview(button)
+        
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
+    
+    @objc func changeLabel() {
+        print("Button pressed")
     }
 
 
