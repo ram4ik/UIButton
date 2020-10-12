94c2ead5d56f82d6b4cc5ac75ceb51b804e7898c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-12 13:42:04 +0300

    textBox & label

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -19,6 +19,25 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(changeLabel), for: .touchUpInside)
         return button
     }()
+    
+    var textBox: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter text"
+        tf.textAlignment = .center
+        tf.layer.borderWidth = 1
+        tf.layer.cornerRadius = 25
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Please enter text"
+        label.font = UIFont.boldSystemFont(ofSize: 35)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +47,27 @@ class ViewController: UIViewController {
     
     func setUI() {
         view.addSubview(button)
+        view.addSubview(textBox)
+        view.addSubview(label)
         
         button.widthAnchor.constraint(equalToConstant: 100).isActive = true
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
         button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
+        textBox.widthAnchor.constraint(equalToConstant: view.frame.width - 20).isActive = true
+        textBox.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        textBox.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textBox.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -10).isActive = true
+        
+        label.bottomAnchor.constraint(equalTo: textBox.topAnchor, constant: -100).isActive = true
+        label.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
     }
     
     @objc func changeLabel() {
         print("Button pressed")
+        label.text = textBox.text
+        textBox.text = ""
     }
 
 
